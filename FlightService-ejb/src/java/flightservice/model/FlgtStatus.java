307ba657af0da778092c6ea850d9877564dedc85
TomBitDe <tbit455674@gmail.com>
2019-01-27 13:25:03 +0100

    Better Documentation.

--- FlightService-ejb/src/java/flightservice/model/FlgtStatus.java
+++ FlightService-ejb/src/java/flightservice/model/FlgtStatus.java
@@ -4,14 +4,42 @@ package flightservice.model;
  * A flight status can be delayed, canceled...
  */
 public enum FlgtStatus {
+
+    /**
+     * Flight is on time
+     */
     OnTime("On time"),
+    /**
+     * Flight arrived
+     */
     Arrived("Arrived"),
+    /**
+     * Plane is taxiing
+     */
     Taxiing("Taxiing"),
+    /**
+     * Boarding started
+     */
     Boarding("Boarding"),
+    /**
+     * Boarding finished
+     */
     EndOfBoarding("Boarding finished"),
+    /**
+     * Flight departed
+     */
     Departed("Departed"),
+    /**
+     * Flight has delay
+     */
     Delayed("Delayed"),
+    /**
+     * Flight is canceled
+     */
     Canceled("Canceled"),
+    /**
+     * Status is unknown
+     */
     Unknown("Unknown");
 
     private final String statusText;
@@ -20,6 +48,13 @@ public enum FlgtStatus {
         this.statusText = statusText;
     }
 
+    /**
+     * Check if this status text equals an other text
+     *
+     * @param otherText the other text
+     *
+     * @return true if it equals, else false
+     */
     public boolean equalsName(String otherText) {
         // (otherName == null) check is not needed because name.equals(null) returns false
         return statusText.equals(otherText);
