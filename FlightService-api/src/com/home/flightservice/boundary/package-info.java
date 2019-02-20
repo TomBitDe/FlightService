d0f36af4c7f28a28f1f42bd24d958c68f20488f4
TomBitDe <tbit455674@gmail.com>
2019-02-20 18:18:48 +0100

    Documentation extended.

--- FlightService-api/src/com/home/flightservice/boundary/package-info.java
+++ FlightService-api/src/com/home/flightservice/boundary/package-info.java
@@ -1,6 +1,9 @@
 /**
  * Flight display Value Object classes.<br>
  * <br>
- * The Value Objects are the response on RESTful Flight Service calls.
+ * The Value Objects are the response on RESTful Flight Service calls.<br>
+ * InfoDisplayVO contains data valid for arrivals and departures.<br>
+ * ArrivalVO contains data valid only for arrivals.<br>
+ * DepartureVO contains data valid only for departures.<br>
  */
 package com.home.flightservice.boundary;
